1a0443d76fe3b434feb952fee770e986c7692984
LeoGuimas <gleo.dev0@gmail.com>
2024-11-01 18:08:22 -0300

    feat: h2 database, test profile, JPA configuration

--- src/main/java/com/spproject/springservices/SpringservicesApplication.java
+++ src/main/java/com/spproject/springservices/SpringservicesApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringservicesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringservicesApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SpringservicesApplication.class, args);
+    }
 }
