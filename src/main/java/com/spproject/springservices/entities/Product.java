e4a8277678ecc764cc7d447fa7bbdf400ea2e7f7
LeoGuimas <gleo.dev0@gmail.com>
2024-11-09 19:40:56 -0300

    feat: add one-to-many relationship between Product and OrderItem; include method to retrieve associated Orders

--- src/main/java/com/spproject/springservices/entities/Product.java
+++ src/main/java/com/spproject/springservices/entities/Product.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Entity;
@@ -11,6 +13,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.JoinTable;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -40,6 +43,10 @@ public class Product implements Serializable {
         this.imgUrl = imgUrl;
     }
 
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
+    //Constructor
     public Product() {
     }
 
@@ -98,6 +105,15 @@ public class Product implements Serializable {
     public Set<Category> getCategories() {
         return categories;
     }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for(OrderItem x : items){
+            set.add(x.getOrder());
+        }
+        return set;
+    }   
 
     @Override
     public int hashCode() {
