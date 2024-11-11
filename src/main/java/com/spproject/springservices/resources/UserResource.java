816ed25a4328dd10e00b9a70ef5630bf4f4f08bc
LeoGuimas <gleo.dev0@gmail.com>
2024-11-11 20:33:09 -0300

    feat: add user creation endpoint in UserResource and corresponding service method

--- src/main/java/com/spproject/springservices/resources/UserResource.java
+++ src/main/java/com/spproject/springservices/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.spproject.springservices.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.spproject.springservices.entities.User;
 import com.spproject.springservices.services.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 @RestController
 @RequestMapping(value = "/users")
@@ -30,4 +36,13 @@ public class UserResource {
         User obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User obj){
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().
+        path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).body(obj);
+    }
+        
 }
