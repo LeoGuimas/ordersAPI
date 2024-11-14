f37e0d95141db9f5d370a48d2982c229d40d2147
LeoGuimas <gleo.dev0@gmail.com>
2024-11-14 10:55:27 -0300

    feat: add exception handling for user update in UserService to manage entity not found scenarios

--- src/main/java/com/spproject/springservices/services/UserService.java
+++ src/main/java/com/spproject/springservices/services/UserService.java
@@ -11,6 +11,8 @@ import com.spproject.springservices.repositories.UserRepository;
 import com.spproject.springservices.services.exceptions.DatabaseException;
 import com.spproject.springservices.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -46,9 +48,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
+        try{
         User entity = repository.getReferenceById(id);
         updateData(entity, obj);
         return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
